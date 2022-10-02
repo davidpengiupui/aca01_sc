8da37c459c55bed3aada6f229268b287e282f251
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2022-10-02 17:57:59 -0400

    Update Unit.h

--- Unit.h
+++ Unit.h
@@ -1,1 +1,16 @@
 #include<iostream>
+
+using namespace std;
+
+class Unit
+{
+    public:
+        int valid = 0;
+        int dirty = 0; 
+        int tag = 0;
+        int addr = 0;
+        inr lru_count = 0;
+
+
+        Unit(int val, int dir, int tag, int adr, int luc);
+};
